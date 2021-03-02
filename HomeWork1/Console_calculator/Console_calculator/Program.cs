ac2ad183e36f40b787dbe079caef23794fd82410
hzh12131108 <59919592+hzh12131108@users.noreply.github.com>
2021-03-02 23:16:06 +0800

    Add files via upload

---
+++ HomeWork1/Console_calculator/Console_calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = ""; 
+            int n1 = 0;
+            int n2 = 0;
+            Console.Write("Please input an operator:");
+            s = Console.ReadLine();
+            Console.Write("Please input the first operand:");
+            n1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Please input the second operand:");
+            n2 = Int32.Parse(Console.ReadLine());
+            if (s =="+")
+                Console.Write($"result:{n1 + n2}");
+            else if(s =="-")
+                Console.Write($"result:{n1 - n2}");
+            else if(s=="*")
+                Console.Write($"result:{n1 * n2}");
+            else if(s=="/")
+                Console.Write($"result:{n1 / n2}");
+            
+        }
+    }
+}
