160c52db79f0122cf66093299053b57ad9e480bb
hzh12131108 <59919592+hzh12131108@users.noreply.github.com>
2021-05-20 21:21:11 +0800

    Add files via upload

---
+++ HomeWork12/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork12.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+        public String ClientName { get; set; }
+       /* public double OrderPrice
+        {
+            get
+            {
+                double sum = 0;
+                foreach (OrderDetail o in OrderDetails)
+                {
+                    sum += o.OrderDetailsPrice;
+                }
+                return sum;
+            }
+            set { }
+        }*/
+      
+    }
+}
---
+++ HomeWork12/Models/OrderDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork12.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+
+        public String goodName { get; set; }
+        public double goodPrice { get; set; }
+        public int count { get; set; }
+        /*public double OrderDetailsPrice { get; set; }*/
+
+        public int OrderID { get; set; }
+        public Order Order { get; set; }
+    }
+}
